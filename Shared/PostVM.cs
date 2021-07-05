711662897917bdcf163d6268ae72a03c19c10574
Tyler Duffus <tyled94@hotmail.com>
2021-07-05 18:31:17 +1000

    Removed Thread property from PostVM

--- Shared/PostVM.cs
+++ Shared/PostVM.cs
@@ -5,7 +5,6 @@ namespace WebForums.Shared
 	public class PostVM
 	{
 		public int ID { get; set; }
-		public ThreadVM Thread { get; set; }
 		public string Content { get; set; }
 		public UserVM Poster { get; set; }
 		public DateTime Created { get; set; }
